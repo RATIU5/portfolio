12a686602339400236733b4d5a18525694122a68
RATIU5 <jcmemmott20@outlook.com>
2023-09-15 19:24:37 -0600

    add: reversed drips

--- src/lib/drips.ts
+++ src/lib/drips.ts
@@ -6,6 +6,7 @@ export function renderDrip(
     width: number;
     height: number;
     invert: boolean;
+    reverse: boolean;
   },
 ) {
   ctx.fillStyle = "#24272E";
@@ -16,10 +17,25 @@ export function renderDrip(
   let radius = width / 2;
 
   ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x + width, y);
-  ctx.arc(x + radius, y + height, radius, 0, Math.PI, options.invert);
-  ctx.lineTo(x, y + height);
+  if (options?.reverse) {
+    ctx.moveTo(x, y + height); // Start from bottom left
+    ctx.arc(
+      x + radius,
+      y + radius,
+      radius,
+      Math.PI,
+      2 * Math.PI,
+      options.invert,
+    ); // Draw the arc
+
+    ctx.lineTo(x + width, y + height); // If not inverted, finish the line to bottom right
+    ctx.lineTo(x, y + height);
+  } else {
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + width, y);
+    ctx.arc(x + radius, y + height, radius, 0, Math.PI, options.invert);
+    ctx.lineTo(x, y + height);
+  }
   ctx.closePath();
   ctx.fill();
 }
@@ -47,23 +63,33 @@ export function createDrip(options: DripOptions) {
 export type DripsOptions = {
   dripWidth: number;
   containerWidth: number;
+  reverse: boolean;
 };
 
 export function createDrips(options: DripsOptions) {
   const array = [];
   for (let i = 0; i < options.containerWidth; i += options.dripWidth) {
-    let height = 0;
-    if (i % 200 === 0) {
-      height = Math.floor(Math.random() * (300 - 215 + 1)) + 215;
+    let val = 0;
+    if (options.reverse) {
+      if (i % 200 === 0) {
+        val = Math.floor(Math.random() * (300 - 215 + 1)) + 215;
+      } else {
+        val = Math.floor(Math.random() * (150 - 50 + 1)) + 50;
+      }
     } else {
-      height = Math.floor(Math.random() * (150 - 50 + 1)) + 50;
+      if (i % 200 === 0) {
+        val = Math.floor(Math.random() * (300 - 215 + 1)) + 215;
+      } else {
+        val = Math.floor(Math.random() * (150 - 50 + 1)) + 50;
+      }
     }
+
     array.push(
       createDrip({
         width: 100,
-        height: height,
+        height: options.reverse ? 200 : val,
         x: i,
-        y: 0,
+        y: options.reverse ? val : 0,
         invert: i % 200 !== 0,
         angle: Math.random() * 2 * Math.PI,
       }),
