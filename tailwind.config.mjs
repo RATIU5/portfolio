f7bee16d8577c4f71698881b8910a4885e9a7526
RATIU5 <jcmemmott20@outlook.com>
2024-01-24 15:06:45 -0700

    initial commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,28 +3,28 @@ const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Lato", ...defaultTheme.fontFamily.sans],
-				serif: ["Literata Variable", ...defaultTheme.fontFamily.serif],
-			},
-			screens: {
-				xs: "480px",
-				// => @media (min-width: 992px) { ... }
-			},
-		},
-	},
-	plugins: [
-		plugin(({ addUtilities }) => {
-			const newUtilities = {
-				".opsz": {
-					"font-variation-settings": '"opsz" 72',
-				},
-			};
-			addUtilities(newUtilities);
-		}),
-		require("@tailwindcss/typography"),
-	],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Lato", ...defaultTheme.fontFamily.sans],
+        serif: ["Literata Variable", ...defaultTheme.fontFamily.serif],
+      },
+      screens: {
+        xs: "480px",
+        // => @media (min-width: 992px) { ... }
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      const newUtilities = {
+        ".opsz": {
+          "font-variation-settings": '"opsz" 72',
+        },
+      };
+      addUtilities(newUtilities);
+    }),
+    require("@tailwindcss/typography"),
+  ],
 };
