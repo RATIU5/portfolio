fffc44556056b46c0d5f2d083c67fd31d5715c9e
RATIU5 <jcmemmott20@outlook.com>
2024-01-26 13:55:47 -0700

    add: project card component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), astroImageTools],
   prefetch: {
     prefetchAll: true,
   },
