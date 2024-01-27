7758a56636d31035f83634a5b967dceac5d88457
RATIU5 <jcmemmott20@outlook.com>
2024-01-27 09:29:39 -0700

    add: first article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import { astroImageTools } from "astro-imagetools";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), icon(), astroImageTools],
+	integrations: [tailwind(), icon()],
 	prefetch: {
 		prefetchAll: true,
 	},
